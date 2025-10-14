645d0be498139c9e2fbeb61fbec92fa457cf11f1
HaytamNajam26 <haytamnajam14@gmail.com>
2025-10-14 21:58:54 +0100

    tp3

---
+++ tp2-main/README.md
@@ -0,0 +1,1 @@
+<img width="1280" height="764" alt="Screenshot 2025-10-14 165232" src="https://github.com/user-attachments/assets/a4b76bae-0cfe-4f85-9640-d2ce860b4d22" />
---
+++ tp2-main/src/main/java/com/example/dao/DaoImpl.java
@@ -0,0 +1,15 @@
+package com.example.dao;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("dao")
+// Déclare cette classe comme un bean Spring avec l'identifiant "dao"
+//@Profile("prod")
+public class DaoImpl implements IDao {
+    @Override
+    public double getValue() {
+        // Cette implémentation retourne une valeur fixe de 100.0
+        return 100.0;
+    }
+}
